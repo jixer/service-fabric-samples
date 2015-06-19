55cd26e9920f7c1aa104f142b6048c210ef825f4
jixer <gsxr.signup@gmail.com>
2015-06-18 23:05:06 -0700

    Initial file -> new stateless service

---
+++ HelloWebApi/HelloWebApi/HelloWebApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Services;
+
+namespace HelloWebApi
+{
+    public class HelloWebApi : StatelessService
+    {
+        protected override ICommunicationListener CreateCommunicationListener()
+        {
+            // TODO: Replace this with an ICommunicationListener implementation if your service needs to handle user requests.
+            return base.CreateCommunicationListener();
+        }
+
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following with your own logic.
+
+            int iterations = 0;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                ServiceEventSource.Current.ServiceMessage(this, "Working-{0}", iterations++);
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
